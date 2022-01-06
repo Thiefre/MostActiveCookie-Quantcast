bf78296b7839f7c628b4a1937c8e715067e7658a
unknown <kevinvluu@gmail.com>
2022-01-05 21:13:27 -0800

    Updated to ameliorate issues from given feedback

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -9,28 +9,36 @@ Created on Thu Dec  9 20:19:01 2021
 # If a DATE is NOT provided, it will display the most active cookie from the entire file; meaning date is disregarded
 # If a DATE is provided, it will display the most active cookie from only that date
 
-import argparse
-
-def most_active_cookies(date, file):
-    
+import argparse  
+from datetime import datetime
+        
+def most_active_cookies(date, file_name):
+            
     # Declare initial variables:
     maximum = 1 # keeps track of the highest amount of cookie occurences 
     table = {} # create a dictionary, or 'hashtable', to map cookies to # of occurences
     
-    try:
-        # Reads each line from the file one by one, parses the information, and updates it into the hashtable
-        for line in file:
+    with open(file_name, 'r') as opened_file: 
+        #Check if date is valid
+        date_obj = date
+        try:
+            date_obj = datetime.strptime(date, "%Y-%m-%d")
+        except ValueError:
+            print(date, "is not a valid date, printing most active cookie disregarding date:")
+            date = ""
+        
+        for line in opened_file:
             # Beginning of string parsing
             cookie, timestamp = line.split(",")
             timestamp = timestamp.strip()
-            if("T" in timestamp):
+            if "T" in timestamp:
                 day, time = timestamp.split("T") # End of string parsing
-                
+                day = datetime.strptime(day, "%Y-%m-%d")
                 # Checks if date of cookie equals the date we are checking, if no date provided in args check everything
-                if(day == date or date == ""):
+                if day == date_obj or date == "":
                     # Creates or updates table entries, increment key/pair values by 1 if cookie is encountered again
                     value = table.get(cookie)
-                    if(value == None):
+                    if value is None:
                         table[cookie] = 1
                     else:
                         table[cookie] += 1
@@ -38,15 +46,13 @@ def most_active_cookies(date, file):
                         if(table[cookie] > maximum):
                             maximum = table[cookie] # End of file parsing and table updating
                 # If day is 'older' than date, break out since the log file is sorted by time
-                elif(day < date):
+                elif(day < date_obj):
                     break
-    except ValueError:
-        print("Not a valid file!")
                         
     max_keys = [] # Array to hold all of the most active cookies
     # Searches through the hashtable for the most active cookies, checking if the # of occurrences == maximum
     for key in table:
-        if(table[key] == maximum):
+        if table[key] == maximum:
             max_keys.append(key) # Adds that cookie to the resulting array
     
     # Walk through the array to print the most active cookies
@@ -60,22 +66,18 @@ def parse_command_line_args():
     
     parser = argparse.ArgumentParser(description = 'Process date in UTC.')
     parser.add_argument('file', help = "file path of .csv for cookie_log", type = str)
-    parser.add_argument('-d', dest = 'bool_date', action = 'store_true', help = 'boolean identifier for date')
-    parser.add_argument('datetime', nargs= "?", default = "", help = "date in UTC", type = str)
+    parser.add_argument('-d', nargs = "?", default = "", help = 'date in UTC', type = str)
     
     args = parser.parse_args()
     
-    date = args.datetime # initial value of date variable, used to store the date from the CL arg
-    file = open(args.file) # open cookie_logs file, with file name provided by CL arg
-    # Checks if date argument '-d' is given
-    if(args.bool_date is True):
-        date = args.datetime
-    return date, file
+    date = args.d # initial value of date variable, used to store the date from the CL arg
+    file_name = args.file # cookie_logs file, with file name provided by CL arg
+    
+    return date, file_name
 
 def main():
-    date, file = parse_command_line_args()
-    most_active_cookies(date, file)
-    file.close()
+    date, file_name = parse_command_line_args()
+    most_active_cookies(date, file_name)
     
 if __name__ == '__main__':
     main()
--- test_most_active_cookie.py
+++ test_most_active_cookie.py
@@ -14,21 +14,25 @@ from most_active_cookie import most_active_cookies
 
 class TestCookies(unittest.TestCase):
     
-    def test_most_active_cookie(self):
-        f = open('cookie_log.csv')
+    def test_different_dates(self):
+        f = 'cookie_log.csv'
         self.assertEqual(most_active_cookies('2018-12-09',f), ['AtY0laUfhglK3lC7'])
-        f.seek(0,0)
         self.assertEqual(most_active_cookies('2018-12-08',f), ['SAZuXPGUrfbcn5UA', '4sMM2LxV07bPJzwf', 'fbcn5UAVanZf6UtG'])
-        f.seek(0,0)
         self.assertEqual(most_active_cookies('2018-12-07',f), ['4sMM2LxV07bPJzwf'])
-        f.seek(0,0)
         self.assertEqual(most_active_cookies('2018-12-06',f), [])
-        f.seek(0,0)
-        self.assertEqual(most_active_cookies('test',f), [])
-        f.seek(0,0)
-        self.assertEqual(most_active_cookies('test',"test"), [])
-        f.close()   
-        
+    
+    def test_invalid_dates(self):
+        f = 'cookie_log.csv'
+        self.assertEqual(most_active_cookies('test',f), ['AtY0laUfhglK3lC7', 'SAZuXPGUrfbcn5UA', '4sMM2LxV07bPJzwf'])
+        self.assertEqual(most_active_cookies('',f), ['AtY0laUfhglK3lC7', 'SAZuXPGUrfbcn5UA', '4sMM2LxV07bPJzwf'])
+
+    def test_file_inputs(self):
+        f = 'dummy_file.txt'
+        with self.assertRaises(ValueError):
+            most_active_cookies('test',f)
+        with self.assertRaises(FileNotFoundError):
+            most_active_cookies('test', 'test')
+            
 if __name__ == '__main__':
     unittest.main()
     
